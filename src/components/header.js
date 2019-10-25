e9ab7493f76823fdb9a2e4ef4ba3e5a8ef044895
Ben D <benjamin.d928@icloud.com>
2019-10-25 12:41:30 +0100

    Added menu bar to header component

--- src/components/header.js
+++ src/components/header.js
@@ -2,11 +2,23 @@ import React from "react";
 import logo from "./bookr-logo-white.png"
 import "../App.css";
 
+
 function Header() {
+
     return (
         <header style={headerStyle}>
+
             <div class="headerLogo">
                 <img src={logo} alt="logo" width="170" height="170"></img>
+            </div>
+            <div class="menuItems">
+                <a href="">About Us</a>
+            </div>
+            <div class="btn-group">
+                <button class="button">About Us</button>
+                <button class="button">Menu</button>
+                <button class="button">Book a Table</button>
+                <button class="button">Contact Us</button>
             </div>
         </header>
     );
